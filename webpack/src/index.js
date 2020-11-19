d9bb19e9c74dd96aa407e395f0fd299f8a93f5ee
1131446340aa <1131446340@qq.com>
2020-11-19 17:34:14 +0800

    weboack

---
+++ webpack/src/index.js
@@ -0,0 +1,13 @@
+/*
+ * @Descripttion: 
+ * @Date: 2020-07-27 11:28:02
+ * @LastEditors: 黄力豪
+ * @LastEditTime: 2020-07-27 12:39:45
+ */
+import './index.css'
+import img from '../image/dow.jpeg'
+let Img = new Image()
+Img.src=img
+document.querySelector('body').appendChild(Img)
+new Promise()
+console.log(img);
